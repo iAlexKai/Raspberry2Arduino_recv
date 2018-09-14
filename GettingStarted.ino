454bac00f5098296f330858d3b15c8a1c293aec6
Alex <iAlex08@outlook.com>
2018-09-14 17:13:59 +0800

    add and delete some annotation

--- GettingStarted.ino
+++ GettingStarted.ino
@@ -10,7 +10,7 @@
 
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 1;
+bool radioNumber = 1; // the sender raspberry pi should be 0 and this receiver should be 1
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio(7,8);
@@ -113,22 +113,7 @@ void loop() {
     
         Serial.print(F("Successfully received message: "));
         Serial.println(gotmsg);  
-     /*
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      //  Serial.println("GOT DATA");
-       // Serial.println(got_time);  
-      }
-     // Serial.println("out");
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
      
-      //Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-    
-     */
    }
  }
 
